f4e29d1a53eb1580939402c5153f782670b57760
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-09-10 20:26:58 +0600

    Initialized

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const userRouters = require("./routes/v1/userRoutes");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.use("/api/v1/users", userRouters);
+
+// if not match all types of HTTP request or existing routes
+app.all("*", (req, res) => {
+  res.send("No route found");
+});
+
+app.listen(port, () => {
+  console.log(`Random User Generator App Listening on Port ${port}`);
+});
